bf467b6a79796c8303fbe92824fcd08d3fe281dd
George <george@immedia.co.za>
2017-08-30 15:42:03 +0200

    initia commit

---
+++ app/Users.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+
+
+    protected $table = 'tbl_users';
+
+   
+      protected $fillable = [
+        'first_name','last_name','user_email', 'user_password',
+    ];
+
+
+}
+
+    
---
+++ app/tbl_users.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tbl_users extends Model
+{
+    //
+}
